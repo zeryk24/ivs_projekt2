8d28342d6c58ede22503f72596fb4c6ce04c20ad
zeryk24 <61562176+zeryk24@users.noreply.github.com>
2020-02-28 15:38:12 +0100

    1. prototyp finálního designu

---
+++ Calculator/Calculator/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Calculator
+{
+	/// <summary>
+	/// Interakční logika pro App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		public App()
+		{
+			
+		}
+	}
+}
---
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+	/// <summary>
+	/// Interakční logika pro MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private string validChars = "0123456789+-*/√";
+		private string operations = "+-*/";
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			App.Current.Resources["MinorColor"] = (Color)ColorConverter.ConvertFromString("#ff0000");
+			App.Current.Resources["MainColor"] = (Color)ColorConverter.ConvertFromString("#FFA500");
+			FocusManager.SetFocusedElement(this, numberTextBox);
+		}
+
+		private void numberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (numberTextBox.Text.Length > 0)
+			{
+				if (!validChars.Contains(numberTextBox.Text.Substring(numberTextBox.Text.Length - 1).ToCharArray()[0]))
+				{
+					numberTextBox.Text = numberTextBox.Text.Substring(0, numberTextBox.Text.Length - 1);
+					numberTextBox.CaretIndex = numberTextBox.Text.Length;
+					MessageBox.Show("Nice Try", "Invalid");
+				}
+				else if (operations.Contains(numberTextBox.Text.Substring(numberTextBox.Text.Length - 1).ToCharArray()[0]) && operations.Contains(numberTextBox.Text.Substring(numberTextBox.Text.Length - 2).ToCharArray()[0]))
+				{
+					numberTextBox.Text = numberTextBox.Text.Substring(0, numberTextBox.Text.Length - 1);
+					numberTextBox.CaretIndex = numberTextBox.Text.Length;
+					MessageBox.Show("Nice Try", "Invalid");
+				}				
+			}
+		}
+
+		private void settingsButton_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show(numberTextBox.Text, "Invalid");
+
+			App.Current.Resources["MinorColor"] = (Color)ColorConverter.ConvertFromString("#00695c");
+			App.Current.Resources["MainColor"] = (Color)ColorConverter.ConvertFromString("#37474f");
+		}
+
+		private void closeButton_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+		private void topBar_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+				Application.Current.MainWindow.DragMove();
+		}
+	}
+}
