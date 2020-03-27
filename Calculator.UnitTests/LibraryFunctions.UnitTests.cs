c19baddde4d81f81a31212d7a97e3228594aa00b
Michal Zavadil <misa.zavadil@gmail.com>
2020-03-27 20:11:58 +0100

    Jednoduche mat. funkce + testy k nim

---
+++ Calculator.UnitTests/LibraryFunctions.UnitTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MatbLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.UnitTests
+{
+    [TestClass]
+    public class LibraryTests
+    {
+        [TestMethod]
+        public void Addition()
+        {
+            Assert.AreEqual(7, Functions.Add(3, 4));
+        }
+        [TestMethod]
+        public void Subtraction()
+        {
+            Assert.AreEqual(6, Functions.Sub(10, 4));
+        }
+        [TestMethod]
+        public void Muliplication()
+        {
+            Assert.AreEqual(72, Functions.Mul(8, 9));
+        }
+        [TestMethod]
+        public void Division()
+        {
+            Assert.AreEqual(3, Functions.Div(15, 5));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivideByZero()
+        {
+            Functions.Div(10, 0);
+        }
+    }
+}
---
+++ MatbLibrary/Funtions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatbLibrary
+{
+    public static class Functions
+    {
+        public static int Add(int x, int y)
+        { 
+            return x + y;
+        }
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Mul(int x, int y)
+        {
+            return x * y;
+        }
+        public static int Div(int x, int y)
+        {
+            try
+            {
+                 return x / y;
+            }
+            catch (DivideByZeroException)
+            {
+                throw;
+            }
+        }
+    }
+}
