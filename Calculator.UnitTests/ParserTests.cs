d07aa55b6daffbc792f410c5d68b8b2aeefa071b
Jan Škvařil <kondr4@seznam.cz>
2020-03-29 19:20:19 +0200

    Testy na parser
    Přidány testy na parser

--- Calculator.UnitTests/ParserTests.cs
+++ Calculator.UnitTests/ParserTests.cs
@@ -10,7 +10,60 @@ namespace Calculator.UnitTests
         [TestMethod]
         public void Validation_RightInput()
         {
-            
+            Assert.IsTrue(Parser.Validate("-5+3*-10-2"));
+            Assert.IsTrue(Parser.Validate("(-5+3)*-10-2"));
+            Assert.IsTrue(Parser.Validate("(-5+3)*-10√2"));
+            Assert.IsTrue(Parser.Validate("(-5+3)*-2!"));
+            Assert.IsTrue(Parser.Validate("(-5+3)*-10^2"));
+        }
+
+        [TestMethod]
+        public void Validation_WrongInput()
+        {
+            Assert.IsFalse(Parser.Validate("(-5)!"));
+            Assert.IsFalse(Parser.Validate("5√-2"));
+            Assert.IsFalse(Parser.Validate("5*!"));
+            Assert.IsFalse(Parser.Validate("5-"));
+            Assert.IsFalse(Parser.Validate("5^"));
+            Assert.IsFalse(Parser.Validate("^5"));
+            Assert.IsFalse(Parser.Validate("5-^5"));
+        }
+
+        [TestMethod]
+        public void SolveOperations()
+        {
+         
+            Assert.AreEqual(Parser.Solve("5+3"), 8);
+            Assert.AreEqual(Parser.Solve("5-3"), 2);
+            Assert.AreEqual(Parser.Solve("-5-3"), -8);
+
+           //Neimplementovano: Assert.AreEqual(Parser.Solve("!5"), 120);
+
+            Assert.AreEqual(Parser.Solve("5*3"), 15);
+            Assert.AreEqual(Parser.Solve("5*-3"), -15);
+            Assert.AreEqual(Parser.Solve("-5*-3"), 15);
+            Assert.AreEqual(Parser.Solve("10%2"), 5);
+            Assert.AreEqual(Parser.Solve("10%-2"), -5);
+
+            /* Neimplementovano
+            Assert.AreEqual(Parser.Solve("2^2"), 4);
+            Assert.AreEqual(Parser.Solve("-(2^2)"), -4);
+            Assert.AreEqual(Parser.Solve("9√3"), 3);
+            */
+        }
+
+
+        [TestMethod]
+        public void SolveOperationOrder()
+        {
+           
+            Assert.AreEqual(Parser.Solve("3-5*2"), -7);
+            Assert.AreEqual(Parser.Solve("(3-5)*2"), -4);
+
+            // neimplementovano
+            //Assert.AreEqual(Parser.Solve("!5*2"), 240);
+            //Assert.AreEqual(Parser.Solve("3-5^2*2"), -47);
+
         }
     }
 }
