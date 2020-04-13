929d7b684181fba5a065cce0c22ec72573d539db
Michal Zavadil <misa.zavadil@gmail.com>
2020-04-13 19:47:16 +0200

    přidány komentáře pro dokumentaci

--- MatbLibrary/Funtions.cs
+++ MatbLibrary/Funtions.cs
@@ -1,29 +1,74 @@
-﻿using System;
+﻿/********************************************************************
+ * Project name: ivs_projekt2
+ * File: Functions.cs
+ * Author: Michal Zavadil xzavad18@fit.vutbr.cz
+ * ******************************************************************/
+/**
+ * @file Functions.cs
+ * @brief class with math library functions
+ * @author Michal Zavadil xzavad18@fit.vutbr.cz
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MatbLibrary
 {
+    /**
+     * Class containing math library functions
+     */
     public static class Functions
     {
+        /**
+         * @brief addition
+         * @param x first operand
+         * @param y second operand
+         * @return sum of 2 numbers
+         */
         public static double Add(double x, double y)
         { 
             return x + y;
         }
+        /**
+         * @brief subtraction
+         * @param x first operand
+         * @param y second operand
+         * @return difference of 2 numbers
+         */
         public static double Sub(double x, double y)
         {
             return x - y;
         }
+        /**
+         * @brief multiplication
+         * @param x first operand
+         * @param y second operand
+         * @return product of 2 numbers
+         */
         public static double Mul(double x, double y)
         {
             return x * y;
         }
+        /**
+         * @brief division
+         * @param x first operand
+         * @param y second operand
+         * @return quotient of 2 numbers
+         * @throw DivideByZeroException if second operand is 0
+         */
         public static double Div(double x, double y)
         {
             if (y == 0)
                 throw new DivideByZeroException("Error: cannot divide by zero");
             return x / y;
         }
+        /**
+         * @brief factorial
+         * @param x number whose factorial we want to find
+         * @return factorial of x
+         * @throw ArgumentException if x is negative or not a whole number
+         */
         public static double Fact(double x)
         {
             if (x < 0 || x % 1 != 0)
@@ -37,6 +82,13 @@ namespace MatbLibrary
             }
             return sum;
         }
+        /**
+         * @brief x to the power of y
+         * @param x base
+         * @param y exponent
+         * @return x to the power of y
+         * @throw ArgumentException if y is not a whole number
+         */
         public static double Exp(double x, double y)
         {
             if (y % 1 != 0)
@@ -60,9 +112,16 @@ namespace MatbLibrary
                 sum = 1 / sum;
             return sum;
         }
-        //nth root algorithm using Newthon's method
-        //x - root degree
-        //y - root base
+        /**
+         * Xth root of y using Newton's method.
+         * @brief xth root of y
+         * @param x root degree
+         * @param y number whose root we want to find 
+         * @return xth root of y
+         * @throw ArgumentException if x is 0
+         * @throw ArgumentException if x is not a whole number
+         * @throw ArgumentException if y is negative and x is odd
+         */
         public static double Root(double x, double y)
         {
             if (x == 0 || x % 1 != 0 || (y < 0 && x % 2 == 0))
@@ -81,7 +140,13 @@ namespace MatbLibrary
 
             return xNext;
         }
-        //NatLog using Taylor series
+        /**
+         * Natural logarithm evaluation using Talor series.
+         * @brief natural logarithm (base equals e)
+         * @param x power, e^a=x
+         * @return exponent 
+         * @throw ArgumentException if x is not positive
+         */
         public static double NatLog(double x)
         {
             if (x <= 0)
@@ -102,3 +167,4 @@ namespace MatbLibrary
         }
     }
 }
+/*** End of file Functions.cs ***/
